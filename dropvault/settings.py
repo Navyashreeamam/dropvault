0bdacc38f6adbda46ade61c986baca0f059ebc06
Navyashree AM <139547683+Navyashreeamam@users.noreply.github.com>
2025-11-11 20:55:43 +0530

    fix: remove base import, simplify settings

--- dropvault/settings.py
+++ dropvault/settings.py
@@ -3,7 +3,7 @@ from decouple import config
 
 import os
 import dj_database_url
-from .base import *
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
