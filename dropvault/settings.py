86603d730677e705ccae70f5d23b6df43680fb0d
Navyashree AM <139547683+Navyashreeamam@users.noreply.github.com>
2025-12-28 09:34:15 +0530

    Add frontend URL to CORS

--- dropvault/settings.py
+++ dropvault/settings.py
@@ -298,6 +298,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "https://dropvaultnew-frontend.onrender.com/",
+
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -309,7 +311,6 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "https://*.onrender.com",
-    "https://*.railway.app",
 ]
 
 # Add Render URL if available
