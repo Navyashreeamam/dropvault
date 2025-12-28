4abb9ea2a35aa8004da90fa4d6d73b278a69c492
Navyashree AM <139547683+Navyashreeamam@users.noreply.github.com>
2025-12-28 20:00:36 +0530

    updateed settings for frontedn

--- dropvault/settings.py
+++ dropvault/settings.py
@@ -298,7 +298,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "https://dropvaultnew-frontend.onrender.com/",
+    "https://dropvault-frontend.onrender.com",
+    ##"https://dropvaultnew-frontend.onrender.com/",
 
 ]
 
